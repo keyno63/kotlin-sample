fc1415af781a694760168872e8abcd9bde237d77
Keyno <kofujiw@gmail.com>
2019-09-15 17:32:49 +0900

    add sample code without arrow-kt pattern.

--- src/main/java/com/github/keyno63/kotlin_sample/arrow/arrowkt_sample.kt
+++ src/main/java/com/github/keyno63/kotlin_sample/arrow/arrowkt_sample.kt
@@ -1,9 +1,15 @@
 package com.github.keyno63.kotlin_sample.arrow
 
 fun main(args: Array<String>) {
-    val nullables: String? = null
-    println(if (nullables != null) nullables.length else 0)
 
+    // case of non-null-type String.
     val sample = "sample"
     println(sample.length)
+
+    // case of null-type String.
+    val nullables: String? = null
+    // must check null or not.
+    println(if (nullables != null) nullables.length else 0)
+
+
 }
