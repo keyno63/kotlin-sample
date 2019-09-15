38d6175c51a91cf6ba1d6053d189b426812a2c54
Keyno <kofujiw@gmail.com>
2019-09-15 17:14:34 +0900

    optional sample.

--- src/main/java/com/github/keyno63/kotlin_sample/arrow/option_sample.kt
+++ src/main/java/com/github/keyno63/kotlin_sample/arrow/option_sample.kt
@@ -5,26 +5,54 @@ import arrow.core.Option
 import arrow.core.Some
 import arrow.core.getOrElse
 
-fun stringDouble(src: String) = src + src
+// 動作確認用関数.
+fun main(args: Array<String>) {
 
-fun sometimeReturnNull(): String? = TODO()
+    // optionResult.
+    val x = Optionals().optionResult
+    when(x) {
+        is Some -> println(x)
+        is None -> println("nothing to print, value is None.")
+        else -> println("invalid type")
+    }
 
-fun sometimeReturnNone(src: String): Option<String> = Option(src)
+    // whenResult
+    println(Optionals().whenResult)
 
-val optionResult: Option<String> = Option.fromNullable(sometimeReturnNull())
+    // foldResult
+    println(Optionals().foldResult)
 
-val whenResult: String = when (optionResult) {
-    None -> "default value"
-    is Some -> optionResult.t
+    // mapResult
+    println(Optionals().mapResult)
+
 }
 
-val foldResult: String = optionResult.fold(
-    ifEmpty = { "default value" },
-    ifSome = { it }
-)
+class Optionals {
+    fun stringDouble(src: String) = src + src
 
-val getOrElseResult: String = optionResult.getOrElse { "default value" }
+    private var someValue: Int = 0
+    fun sometimeReturnNull(): String? {
+        //someValue += 1
+        return if (someValue % 2 == 0) "Something." else null
+    }
 
-val mapResult: Option<String> = optionResult.map(::stringDouble)
+    fun sometimeReturnNone(src: String): Option<String> = Option(src)
 
-val flatMapResult: Option<String> = optionResult.flatMap(::sometimeReturnNone)
+    val optionResult: Option<String> = Option.fromNullable(sometimeReturnNull())
+
+    val whenResult: String = when (optionResult) {
+        None -> "default value"
+        is Some -> optionResult.t
+    }
+
+    val foldResult: String = optionResult.fold(
+        ifEmpty = { "default value" },
+        ifSome = { it }
+    )
+
+    val getOrElseResult: String = optionResult.getOrElse { "default value" }
+
+    val mapResult: Option<String> = optionResult.map(::stringDouble)
+
+    val flatMapResult: Option<String> = optionResult.flatMap(::sometimeReturnNone)
+}
